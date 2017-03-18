2d36f473fa6a9ee39fb371d0a5ee80f847ddb849
hzxiongshiqiang <hzxiongshiqiang@corp.netease.com>
2017-03-18 11:26:42 +0800

    添加建表语句和线上配置文件

---
+++ src/create_table_monitor.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+create "epay:risk-monitordb-uid-online",{NAME=>'id'},{NAME =>'name'}
+create "epay:risk-monitordb-online",{NAME => 't'}
+create "epay:risk-monitordb-tree-online",{NAME => 't'}
+create "epay:risk-monitordb-meta-online",{NAME => 'name'}
